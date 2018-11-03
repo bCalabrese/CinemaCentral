3cff1fac3eb9827a7dc4a76ea8f6e5a991356a5d
bCalabrese <calabresebrandon3@gmail.com>
2018-11-02 23:03:07 -0500

    initial commit + rough login prototype

---
+++ src/account/login.java
@@ -0,0 +1,60 @@
+package account;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String email = request.getParameter("email");
+		String pass = request.getParameter("password");
+		
+		if (email.equals("letmein@email.com") && pass.equals("sesame")) { // TODO: validate credentials with database
+			RequestDispatcher rd=request.getRequestDispatcher("landing.html");
+			rd.forward(request, response);
+		}
+		else { // bad credentials
+			out.print("<p style=\"color:red;\">Incorrect e-mail or password. Please verify your credentials</p>");
+			out.print("\n" + email + "\n" + pass);
+			RequestDispatcher rd=request.getRequestDispatcher("index.html");
+			rd.include(request, response);
+		}
+		out.close();
+	}
+
+}
